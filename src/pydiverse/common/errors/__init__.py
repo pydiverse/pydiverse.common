c063d86f7693b3af452ff8db22cde853b77efdb2
windiana42 <windiana@users.sf.net>
2025-06-06 13:06:24 +0200

    moved many utility functions from `pydiverse.pipedag` to `pydiverse.common`
    this includes deep_map, ComputationTracer, @disposable, @requires, stable_hash,
        load_object, and structlog initialization

---
+++ src/pydiverse/common/errors/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) QuantCo and pydiverse contributors 2025-2025
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+
+class DisposedError(Exception):
+    """
+    Exception raise when an object has been disposed, but some attributes are
+    being accessed nevertheless.
+    """
