1f41ebdab8f82ebef361a452754b2e268945d74c
windiana42 <windiana@users.sf.net>
2025-10-07 13:06:10 +0200

    disable filter_by_level for logging compatibility setup

--- tests/test_structlog.py
+++ tests/test_structlog.py
@@ -48,6 +48,42 @@ def test_structlog_level01():
 
 
 @pytest.mark.skipif(structlog is None, reason="requires structlog")
+def test_structlog_level01b():
+    logger = logging.getLogger(__name__ + ".test_structlog_level01b")
+    logger.setLevel(logging.DEBUG)
+    logger.parent.setLevel(logging.INFO)  # be more strict on parent level
+    logger.info("update cache")
+    with capture_logs() as logs:
+        with log_level(logging.DEBUG):
+            assert logger.isEnabledFor(logging.DEBUG)
+            assert logger.isEnabledFor(logging.INFO)
+            logger.info("This is a test log message: %s", dict(key1="value1", key2=42))
+            logger.debug("This is a debug message: %s", dict(key1="value1", key2=42))
+    assert logs == []  # capture_logs does not work for standard logging
+
+
+@pytest.mark.skipif(structlog is None, reason="requires structlog")
+def test_structlog_level01c():
+    logger = structlog.get_logger(__name__ + ".test_structlog_level01")
+    logger.setLevel(logging.INFO)
+    logger.parent.setLevel(logging.ERROR)  # be more strict on parent level
+    logger.info("update cache")
+    with capture_logs() as logs:
+        with log_level(logging.INFO):
+            logger.debug("This is a debug message", key1="value1", key2=42)
+            logger.info("This is a test log message", key1="value1", key2=42)
+    assert logs == [
+        {
+            "key1": "value1",
+            "key2": 42,
+            "event": "This is a test log message",
+            "level": "info",
+            "log_level": "info",
+        }
+    ]
+
+
+@pytest.mark.skipif(structlog is None, reason="requires structlog")
 def test_structlog_level02():
     logger = structlog.get_logger(__name__ + ".test_structlog_level02")
     with capture_logs() as logs:
